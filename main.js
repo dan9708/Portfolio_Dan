dc9b62ebb293d76308db818fbd9f18e787046ea2
dan9708 <thdgus0829@gmail.com>
2022-01-10 14:52:48 +0900

    js: Scroll to section, contact me btn Handle

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark')
   }
 })
+
+
+// navbar 클릭시 해당 위치로 이동하는 기능
+const navbarMenu = document.querySelector('.navbar__menu')
+navbarMenu.addEventListener ('click', (event) => {
+  const target = event.target
+  const link = target.dataset.link
+  if (link == null) {
+    return
+  }
+
+  // console.log(event.target.dataset.link)
+  // const scrollTo = document.querySelector(link)
+  // scrollTo.scrollIntoView({behavior: 'smooth'})
+  scrollIntoView(link)
+})
+
+// contact me 버튼 클릭시 contact로 위치 이동
+const contactMeBtn = document.querySelector('.home__contact')
+contactMeBtn.addEventListener('click', () => {
+  scrollIntoView('.contact')
+})
+
+function scrollIntoView (selector) {
+  const scrollTo = document.querySelector(selector)
+  scrollTo.scrollIntoView({behavior: 'smooth'})
+}
